0cc15d11b195d4557632a6b6bb51da20abf7815e
McKay Burnett <mckay.burnett@gmail.com>
2023-12-20 19:33:49 -0700

    create page toggle to show last made on and off functional

--- qr-creator/client/src/components/Nav.js
+++ qr-creator/client/src/components/Nav.js
@@ -7,13 +7,18 @@ import {BrowserRouter as Routes, Route, Link} from 'react-router-dom'
 
 export default function Nav(props){
 
-    const { logout, token, toggle, setToggle } = useContext(Context)
+    const { logout, token, toggle, setToggle, setShowLast } = useContext(Context)
+
+    function runToggles(){
+        setToggle(true)
+        setShowLast(false)
+    }
 
     return(
         <div className="nav-wrapper">
-            <Link to='/' className="nav-home" onClick={() => setToggle(true)} classname="nav-home">Home</Link>
+            <Link to='/' className="nav-home" onClick={() => runToggles} classname="nav-home">Home</Link>
             {token ? <Link to='/create' className="nav-create">Create</Link> : <h3 className="nav-create" onClick={() => setToggle(false)}>Create</h3>}
-            {token ? <Link to='/saved' className="nav-saved">Saved</Link> : <h3 className="nav-saved" onClick={() => setToggle(false)}>Saved</h3>}
+            {token ? <Link to='/saved' className="nav-saved" onClick={() => setShowLast(false)}>Saved</Link> : <h3 className="nav-saved" onClick={() => setToggle(false)}>Saved</h3>}
             { token ? 
             <h3 className="nav-logout" onClick={logout}>Logout</h3>
             :
