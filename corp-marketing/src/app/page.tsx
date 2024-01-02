bc02d990c4849436292697cf3aee8b872fd37d13
McKay Burnett <mckay.burnett@gmail.com>
2024-01-02 15:13:45 -0700

    nextjs corp-marketing

---
+++ corp-marketing/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import homeImg from 'public/home.jpg'
+
+export default function Home() {
+  return (
+  <div>
+    Home Page
+    <Image 
+      src={homeImg}
+      alt="car factory"
+    />
+  </div>
+  )
+  
+}
---
+++ corp-marketing/src/app/reliability/page.tsx
@@ -0,0 +1,3 @@
+export default function ReliablityPage(){
+    return <div>Reliability Page</div>
+}
