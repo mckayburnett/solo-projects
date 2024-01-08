895e408043af89fe1af5721453917c5bc5d71092
McKay Burnett <mckay.burnett@gmail.com>
2024-01-08 14:43:14 -0700

    tester

--- currcon/src/Form.js
+++ currcon/src/Form.js
@@ -7,20 +7,12 @@ export default function Form(){
     const tester = (currencies.map(currency => {
         return <input type="radio" value={currency} placeholder={currency} name={currency} onClick={(e) => console.log(e.target.value)}></input>
     }))
-    console.log(tester)
+    console.log('tester',tester)
 
     return(
         <div className="form-wrapper">
             <form className="form-form">
                 Select Currency(s)
-                <input 
-                    className="form-input-currencies"
-                    type="radio"
-                    name="currencies"
-                    placeholder="Select Currency"
-                    value={currencies}
-                >
-                </input>
                 {tester}
             </form>
         </div>
