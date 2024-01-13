04118ffba2840d1baede411cfe8cce27f1ba16a0
McKay Burnett <mckay.burnett@gmail.com>
2024-01-12 17:03:46 -0700

    private, public, get

--- understanding-typescript/app.ts
+++ understanding-typescript/app.ts
@@ -32,8 +32,19 @@ class ITDepartment extends Department { //the ITDepartment will inherit EVERYTHI
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    get mostRecentReport(){ //getter is where you use a method/function to retrieve a value
+        if(this.lastReport){
+            return this.lastReport //getters have to return something. Below you access it with just dot notation, no "()" => accounting.mostRecentReport
+        }
+        throw new Error('No report found.')
+    }
+
+
     constructor(id: string, private reports: string[]) {
         super(id, 'Acounting')
+        this.lastReport = reports[0]
     }
 
     addEmployee(name: string){
@@ -45,6 +56,7 @@ class AccountingDepartment extends Department {
 
     addReport(text: string){
         this.reports.push(text)
+        this.lastReport = text
     }
 
     printReports() {
@@ -63,6 +75,7 @@ it.printEmployeeInformation();
 console.log(it)
 
 const accounting = new AccountingDepartment('d2', []);
+
 
 accounting.addReport('Something went wrong...');
 accounting.printReports();
