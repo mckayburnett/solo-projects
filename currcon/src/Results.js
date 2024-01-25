b98afd2f2642c5c5b03b69a8199bfc5af9e65a9c
McKay Burnett <mckay.burnett@gmail.com>
2024-01-24 20:41:42 -0700

    auto scroll good. new display for results

--- currcon/src/Results.js
+++ currcon/src/Results.js
@@ -6,17 +6,18 @@ export default function Results(props){
     const {currency, getConversion, converted, runFunc} = useContext(Context)
 
     function scroll() {
-      window.scrollTo({ top: 9999, behavior: "smooth" })
+      window.scrollTo({ top: 999, behavior: "smooth" })
       console.log('firing')
     }
+    console.log('window', window.scrollY)
 
     const rates = runFunc && converted && converted.rates ? (
         Object.values(converted.rates).map((data, i) => (
           <h3 className="results-rates-currency"key={currency[i]}>{currency[i]} : {data < 1 ? <span className="results-rates-dataRed">{data}</span>: <span className="results-rates-dataGreen">{data}</span>}</h3>
         )) 
       ) : "... loading";
-
-      scroll()
+      converted.rates && Object.values(converted.rates).length > 1 && scroll()
+      
     return(
         <div className="results-wrapper">
             <h3 className="results-date">Date/Time updated: {converted.date}</h3>
