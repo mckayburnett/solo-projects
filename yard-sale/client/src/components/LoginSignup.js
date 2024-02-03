e53fd4b249919519f68dbdf02b3ecaa185760eaf
McKay Burnett <mckay.burnett@gmail.com>
2024-02-03 17:34:41 -0500

    nav styling good. started signup and login form.

--- yard-sale/client/src/components/LoginSignup.js
+++ yard-sale/client/src/components/LoginSignup.js
@@ -3,6 +3,20 @@ import React, { useContext, useState, useEffect } from 'react'
 export default function LoginSignup(props){
 
     return (
-        <h1>Login/Signup</h1>
+        <div className="login-wrapper">
+            <form className="login-form">
+                <input 
+                    className="login-form-email"
+                    placeholder="Email"
+                    type="email"
+                />
+                <input 
+                    className="login-form-password"
+                    placeholder="Password"
+                    type="password"
+                />
+                {/*token ? */<button className="login-form-button">Login</button> /*: <button className="login-form-button" onClick={() => signup}>Signup</button>*/}
+            </form>
+        </div>
     )
 }
