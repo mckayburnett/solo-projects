ae6279e1c428cb3056ea223d16c0ebd9f4c657b5
McKay Burnett <mckay.burnett@gmail.com>
2024-02-16 23:05:55 -0700

    yardRouter gets

--- yard-sale/routes/yardRouter.js
+++ yard-sale/routes/yardRouter.js
@@ -4,13 +4,37 @@ const Location = require('../models/location')
 const Item = require('../models/item.js')
 
 //token && get all locations
-
+yardRouter.get('/', async (req, res, next) => {
+    try {
+        const location = await Location.find()
+        return res.status(200).send(location)
+    } catch(err) {
+        res.status(500)
+        return next(err)
+    }
+})
 
 //token && user && get locations based on proximity
-
+codeRouter.get('/user', async (req, res, next) => {
+    try {
+        const codes = await Code.find({ user: req.auth._id })
+        res.status(200).send(codes)       
+    } catch(err) {
+        res.status(500)
+        next(err)
+    } 
+})
 
 //token && get items from location id
-
+yardRouter.get('/:locationId', async (req, res, next) => {
+    try {
+        const location = await Code.find({ location: req.params._id })
+        res.status(200).send(codes)       
+    } catch(err) {
+        res.status(500)
+        next(err)
+    } 
+})
 
 //token && user && post location 
 
